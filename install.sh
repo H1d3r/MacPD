9a007a35b450a1ab0d78cf04292c1b5466e2d00d
H1d3r <H1d3r@foxmail.com>
2023-06-25 15:46:19 +0800

    update v18.2.0

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ NOCOLOR='\033[0m'
 
 PDFM_DIR="/Applications/Parallels Desktop.app"
 PDFM_LOC="/Library/Preferences/Parallels/parallels-desktop.loc"
-PDFM_VER="18.0.3-53079"
+PDFM_VER="18.0.1-53056"
 
 PDFM_DISP_CRACK="${BASE_PATH}/prl_disp_service"
 PDFM_DISP_DST="${PDFM_DIR}/Contents/MacOS/Parallels Service.app/Contents/MacOS/prl_disp_service"
@@ -20,7 +20,8 @@ PDFM_DISP_ENT="${BASE_PATH}/ParallelsService.entitlements"
 LICENSE_FILE="${BASE_PATH}/licenses.json"
 LICENSE_DST="/Library/Preferences/Parallels/licenses.json"
 
-PDFM_DISP_HASH="c45b432ed90b55ffef6ae9042f3ca162887d1a21581ae1f200248789d566e060"
+PDFM_DISP_ORIGINAL_HASH="70b92c64c81c7992e901c2e23a2c2a08547e0d82f3b4fa28cc5f7e8bbac04cb6"
+PDFM_DISP_HASH="a0975389fb97f54c831d4db896d9b6983000b05dad4c8db88c3f2aced35a6dd9"
 LICENSE_HASH="ac735f3ee7ac815539f07e68561baceda858cf7ac5887feae863f10a60db3d79"
 
 # read location from parallels-desktop.loc
@@ -51,7 +52,7 @@ if [ "${FILE_HASH}" != "${PDFM_DISP_HASH}" ]; then
   echo -e "${COLOR_ERR}[-] ${FILE_HASH} != ${PDFM_DISP_HASH}${NOCOLOR}"
   echo -e "${COLOR_ERR}[-] verify crack file (prl_disp_service) hash error.${NOCOLOR}"
   echo -e "${COLOR_ERR}[-] please re-download crack files.${NOCOLOR}"
-  exit 3
+  exit 4
 fi
 
 # check licenses.json hash
@@ -63,36 +64,43 @@ if [ "${FILE_HASH}" != "${LICENSE_HASH}" ]; then
   exit 4
 fi
 
-# check root permission
+# check run as root
 if [ "$EUID" -ne 0 ]; then
-  echo -e "${COLOR_ERR}[-] Not have root permission, run sudo.${NOCOLOR}"
-  exec sudo "$0" "$@"
-  exit 5
+  echo -e "${COLOR_ERR}[-] Please run as root.${NOCOLOR}"
+  echo -e "${COLOR_INFO}    eg. sudo ${NOCOLOR}$0"
+  exit 3
 fi
 
-# if prl_disp_service running, stop it
-if pgrep -x "prl_disp_service" &> /dev/null; then
-  echo -e "${COLOR_INFO}[*] Stop Parallels Desktop${NOCOLOR}"
-  pkill -9 prl_client_app &>/dev/null
-  pkill -9 prl_disp_service &>/dev/null
-  launchctl stop /Library/LaunchDaemons/com.parallels.desktop.launchdaemon.plist &>/dev/null
-fi
+echo -e "${COLOR_INFO}[*] Exit Parallels Desktop${NOCOLOR}"
+"${PDFM_DIR}/Contents/MacOS/Parallels Service" service_stop
+killall prl_client_app 2>/dev/null
+
+echo -e "${COLOR_INFO}[*] Start Parallels Service${NOCOLOR}"
+"${PDFM_DIR}/Contents/MacOS/Parallels Service" service_start
+
+echo -e "${COLOR_INFO}[*] Exit Parallels Desktop account ...${NOCOLOR}"
+"${PDFM_DIR}/Contents/MacOS/prlsrvctl" web-portal signout 2>/dev/null
+
+echo -e "${COLOR_INFO}[*] Disable CEP ...${NOCOLOR}"
+"${PDFM_DIR}/Contents/MacOS/prlsrvctl" --cep off 2>/dev/null
+"${PDFM_DIR}/Contents/MacOS/prlsrvctl" --allow-attach-screenshots off 2>/dev/null
+
+echo -e "${COLOR_INFO}[*] Stop Parallels Service${NOCOLOR}"
+"${PDFM_DIR}/Contents/MacOS/Parallels Service" service_stop
 
 echo -e "${COLOR_INFO}[*] Copy prl_disp_service${NOCOLOR}"
 
-chflags -R 0 "${PDFM_DISP_DST}" || { echo -e "${COLOR_ERR}error $? at line $LINENO.${NOCOLOR}"; exit $?; }
 rm -f "${PDFM_DISP_DST}" || { echo -e "${COLOR_ERR}error $? at line $LINENO.${NOCOLOR}"; exit $?; }
-cp "${PDFM_DISP_CRACK}" "${PDFM_DISP_DST}" || { echo -e "${COLOR_ERR}error $? at line $LINENO.${NOCOLOR}"; exit $?; }
+cp -X "${PDFM_DISP_CRACK}" "${PDFM_DISP_DST}" || { echo -e "${COLOR_ERR}error $? at line $LINENO.${NOCOLOR}"; exit $?; }
 chown root:wheel "${PDFM_DISP_DST}" || { echo -e "${COLOR_ERR}error $? at line $LINENO.${NOCOLOR}"; exit $?; }
 chmod 755 "${PDFM_DISP_DST}" || { echo -e "${COLOR_ERR}error $? at line $LINENO.${NOCOLOR}"; exit $?; }
-chflags -R 0 "${PDFM_DISP_CRACK}" || { echo -e "${COLOR_ERR}error $? at line $LINENO.${NOCOLOR}"; exit $?; }
 
 # check hash
 FILE_HASH=$(shasum -a 256 -b "${PDFM_DISP_DST}" | awk '{print $1}')
 if [ "${FILE_HASH}" != "${PDFM_DISP_HASH}" ]; then
   echo -e "${COLOR_ERR}[-] ${FILE_HASH} != ${PDFM_DISP_HASH}${NOCOLOR}"
   echo -e "${COLOR_ERR}[-] verify target file (prl_disp_service) hash error.${NOCOLOR}"
-  exit 6
+  exit 4
 fi
 
 echo -e "${COLOR_INFO}[*] Sign prl_disp_service${NOCOLOR}"
@@ -106,10 +114,9 @@ if [ -f "${LICENSE_DST}" ]; then
   rm -f "${LICENSE_DST}" > /dev/null || { echo -e "${COLOR_ERR}error $? at line $LINENO.${NOCOLOR}"; exit $?; }
 fi
 
-cp -f "${LICENSE_FILE}" "${LICENSE_DST}" || { echo -e "${COLOR_ERR}error $? at line $LINENO.${NOCOLOR}"; exit $?; }
+cp -X "${LICENSE_FILE}" "${LICENSE_DST}" || { echo -e "${COLOR_ERR}error $? at line $LINENO.${NOCOLOR}"; exit $?; }
 chown root:wheel "${LICENSE_DST}" || { echo -e "${COLOR_ERR}error $? at line $LINENO.${NOCOLOR}"; exit $?; }
 chmod 444 "${LICENSE_DST}" || { echo -e "${COLOR_ERR}error $? at line $LINENO.${NOCOLOR}"; exit $?; }
-chflags -R 0 "${LICENSE_DST}" || { echo -e "${COLOR_ERR}error $? at line $LINENO.${NOCOLOR}"; exit $?; }
 chflags uchg "${LICENSE_DST}" || { echo -e "${COLOR_ERR}error $? at line $LINENO.${NOCOLOR}"; exit $?; }
 chflags schg "${LICENSE_DST}" || { echo -e "${COLOR_ERR}error $? at line $LINENO.${NOCOLOR}"; exit $?; }
 
@@ -118,39 +125,7 @@ FILE_HASH=$(shasum -a 256 -b "${LICENSE_DST}" | awk '{print $1}')
 if [ "${FILE_HASH}" != "${LICENSE_HASH}" ]; then
   echo -e "${COLOR_ERR}[-] ${FILE_HASH} != ${LICENSE_HASH}${NOCOLOR}"
   echo -e "${COLOR_ERR}[-] verify target file (${LICENSE_DST}) hash error.${NOCOLOR}"
-  exit 7
+  exit 1
 fi
 
-# is prl_disp_service not running, start it
-if ! pgrep -x "prl_disp_service" &>/dev/null; then
-  echo -e "${COLOR_INFO}[*] Start Parallels Service${NOCOLOR}"
-  "${PDFM_DIR}/Contents/MacOS/Parallels Service" service_restart &>/dev/null
-  for (( i=0; i < 10; ++i ))
-  do
-    if pgrep -x "prl_disp_service" &>/dev/null; then
-      break
-    fi
-    sleep 1
-  done
-  if ! pgrep -x "prl_disp_service" &>/dev/null; then
-    echo -e "${COLOR_ERR}[x] Start Service fail.${NOCOLOR}"
-  fi
-fi
-
-VALID_INFO="License: state='valid' restricted='false'"
-
-"${PDFM_DIR}/Contents/MacOS/prlsrvctl" info | grep "${VALID_INFO}" &>/dev/null
-if [ $? != 0 ]; then
-  echo -e "${COLOR_ERR}[x] Crack fail, please retry it.${NOCOLOR}"
-  exit 9
-fi
-
-echo -e "${COLOR_INFO}[*] Exit Parallels Desktop account ...${NOCOLOR}"
-"${PDFM_DIR}/Contents/MacOS/prlsrvctl" web-portal signout &>/dev/null
-
-echo -e "${COLOR_INFO}[*] Disable CEP ...${NOCOLOR}"
-"${PDFM_DIR}/Contents/MacOS/prlsrvctl" set --cep off &>/dev/null
-"${PDFM_DIR}/Contents/MacOS/prlsrvctl" set --allow-attach-screenshots off &>/dev/null
-
-echo -e "${COLOR_INFO}[*] Crack success.${NOCOLOR}"
-
+echo -e "${COLOR_INFO}[*] Crack over${NOCOLOR}"
--- readme.md
+++ readme.md
@@ -1,48 +1,79 @@
 # Parallels Desktop Crack
 
-Crack for Parallels Desktop 18.0.3 53079
+Crack for Parallels Desktop 18.2.0-53488 破解版  ✅ 已支持 Ventura 13.x
 
-- [x] Support Intel
-- [x] Support Apple Silicon (M1 & M2)
-- [x] Network
-- [x] USB
+##### ✅ - Support Intel
+
+##### ✅ - Support Apple Silicon (M1 & M2)
+
+##### ✅ - Network
+
+##### ✅ - USB
+
+###### 🎉目前该项目由马克喵同步更新维护🎉
+
+# 目前激活工具修复问题
+
+*修复不能先破解后安装镜像的问题，
+
+*修复运行虚拟系统prl_disp_service无法运行问题，
+
+18.2.0官方修复此前破解方式，请等待后续更新破解！请回滚到18.1.1，安装破解
+
+
+# 中文教程：
+
+⚠️ 注意：安装证书的时候要赋予终端权限！否则显示“Operation not permitted”
+
+打开 “完全磁盘访问” 权限.系统偏好设置 ▸ 隐私与安全性 ▸ 完整磁盘访问权限 ☑️勾选了终端 （如果之前勾选过-然后+）
+
+![image](https://user-images.githubusercontent.com/39859514/199421247-b59a9092-a70a-4a70-a3ec-c3e73663f5f5.png)
+
+
+1.去官网下载安装最新版18.1.1，并自己安装完需要的镜像文件
+
+18.1.1官方安装包：https://download.parallels.com/desktop/v18/18.1.1-53328/ParallelsDesktop-18.1.1-53328.dmg
+
+官方PD最新安装包：https://download.parallels.com/desktop/v18/18.2.0-53488/ParallelsDesktop-18.2.0-53488.dmg
+
+2.更新安装完PD后打开一次！
+
+自动安装完pd可能会出现“无法启动”提示，不要慌直接去启动台打开pd即可！
+
+3.下载网站左侧激活工具，点击安装证书即可！打开PD查看吧！
+
+4.一键屏蔽验证Pd联网工具即将发布！QQ群：342929996
+
+
+![image](https://user-images.githubusercontent.com/39859514/207548077-7a2be77d-1137-47e2-bbbf-685abead8256.png)
+![image](https://user-images.githubusercontent.com/39859514/207548105-9e2f704d-6a70-4881-bff3-45e1e92920e8.png)
+
 
 # Usage
 
-1. Install Parallels Desktop.
+1. Install Parallels Desktop 18.1.1-53328.
 
-    https://download.parallels.com/desktop/v18/18.0.3-53079/ParallelsDesktop-18.0.3-53079.dmg
+   https://download.parallels.com/desktop/v18/18.1.1-53328/ParallelsDesktop-18.1.1-53328.dmg
 
 2. Exit parallels account.
 
-3. Download this repo files.
+3. Download this repo file.
 
 4. Extract and run Terminal in this directory.
 
 5. `chmod +x ./install.sh && sudo ./install.sh`
 
-If you got "Operation not permitted" error, enable "Full Disk Access" permission for your Terminal app.
-
-`System Preferences ▸ Security & Privacy ▸ Privacy ▸ Full Disk Access`
-
-If you got `codesign` error, ensure xcode command line tools installed. Install with command `xcode-select --install`.
-
-Check installed with `xcode-select -p` will output `/Library/Developer/CommandLineTools` or `/Applications/Xcode.app/Contents/Developer`.
-
 
 # Manual
 
-1. Open `Parallels Desktop` and exit your account.
-
-2. Exit `Parallels Desktop`.
-
-3. Ensure prl_disp_service not running.
+1. Exit Parallels Desktop
 
 ```
-pkill -9 prl_disp_service
+killall -9 prl_client_app
+killall -9 prl_disp_service
 ```
 
-4. Copy cracked `prl_disp_service` file.
+2. Copy crack file
 
 ```
 sudo cp -f prl_disp_service "/Applications/Parallels Desktop.app/Contents/MacOS/Parallels Service.app/Contents/MacOS/prl_disp_service"
@@ -50,37 +81,34 @@ sudo chown root:wheel "/Applications/Parallels Desktop.app/Contents/MacOS/Parall
 sudo chmod 755 "/Applications/Parallels Desktop.app/Contents/MacOS/Parallels Service.app/Contents/MacOS/prl_disp_service"
 ```
 
-5. Copy fake licenses.json.
+3. Copy licenses.json
 
 ```
-sudo cp -f licenses.json "/Library/Preferences/Parallels/licenses.json"
+sudo rm -f "/Library/Preferences/Parallels/licenses.json"
+sudo cp licenses.json "/Library/Preferences/Parallels/licenses.json"
 sudo chown root:wheel "/Library/Preferences/Parallels/licenses.json"
 sudo chmod 444 "/Library/Preferences/Parallels/licenses.json"
-sudo chflags uchg "/Library/Preferences/Parallels/licenses.json"
-sudo chflags schg "/Library/Preferences/Parallels/licenses.json"
 ```
 
-6. Sign `prl_disp_service` file.
+4. Sign
 
 ```
-sudo codesign -f -s - --timestamp=none --all-architectures --entitlements ParallelsService.entitlements "/Applications/Parallels Desktop.app/Contents/MacOS/Parallels Service.app/Contents/MacOS/prl_disp_service"
+sudo codesign -f -s - --timestamp=none --all-architectures --deep --entitlements ParallelsService.entitlements "/Applications/Parallels Desktop.app/Contents/MacOS/Parallels Service.app/Contents/MacOS/prl_disp_service"
 ```
-
 
 # Notice
 
 Parallels Desktop may upload client info or logs to server.
 
-You can use a firewall, hosts or custom DNS block there domains.
+You can use a firewall block there domains.
 
-This prevents the built-in downloader from working, but you can download prebuilt Virtual Machines via
-* Apple Silicon
-    * https://update.parallels.com/desktop/v18/appliances_arm.xml
-    * https://update.parallels.com/desktop/v18/appliances_arm_Monterey.xml
-* Intel
-    * https://update.parallels.com/desktop/v18/appliances.xml
+Or use Hosts, AdGuardHome filter DNS resolve.
 
-## Hosts
+一键屏蔽验证Pd联网工具即将发布！QQ群：342929996
+工具已发布：https://macmiao.lanzouy.com/iroRC0feprzc
+
+PD正常用时可以不用该工具！该用具只限制PD打开跳试用的！
+
 
 ```
 127.0.0.1 download.parallels.com
@@ -112,18 +140,14 @@ This prevents the built-in downloader from working, but you can download prebuil
 127.0.0.1 myparallels.com
 127.0.0.1 my.parallels.com
 ```
-
-Parallels Desktop will uncomment hosts file, can use this command lock your hosts file:
+Parallels Desktop 将取消注释主机文件，可以使用此命令锁定您的主机文件：
 
 ```
 sudo chflags uchg /etc/hosts
 sudo chflags schg /etc/hosts
 ```
-
-## AdGuardHome
-
-Add the following rules to your `Custom filtering rules`:
-
+AdGuard 主页
+将以下规则添加到您的Custom filtering rules：
 ```
 ||myparallels.com^$important
 ||parallels.cn^$important
@@ -137,22 +161,12 @@ Add the following rules to your `Custom filtering rules`:
 ||parallelskorea.com^$important
 ||parallels.com.cdn.cloudflare.net^$important
 ```
+FAQ
+为什么prl_disp_service文件这么大？
+它是原始prl_disp_service文件的直接补丁文件。
 
+这个破解安全吗？
+它是开源的，你可以使用任何你喜欢的十六进制文件比较工具打开prl_disp_service来查看修改了什么。
 
-# FAQ
-
-## Why `prl_disp_service` file so big?
-
-It's direct patch'd file for original `prl_disp_service` file.
-
-## Is this crack safe?
-
-It's opensource, you can use any hex file comparison tool you like open `prl_disp_service` to see what has been modified.
-
-## I want to crack it myself.
-
-Check the `prl_disp_service.md` to see how I cracked it.
-
-## Where to get update?
-
-[https://icrack.day/pdfm](https://icrack.day/pdfm)
+我想自己破解。
+检查看看prl_disp_service.md我是如何破解它的。
